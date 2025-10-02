974ce6d559eb33d7dcca93d68e3cc0f1d1db5d61
Robani Diansyah <melinting522@gmail.com>
2025-10-02 15:02:26 +0700

    feat: membuat service POST untuk menyimpan data barang

--- src/app/api/barang/route.ts
+++ src/app/api/barang/route.ts
@@ -1,6 +1,6 @@
 
 import { PrismaClient } from "@/generated/prisma";
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 // Buat variable prisma (PrismaClient)
 const prisma = new PrismaClient();
@@ -17,14 +17,35 @@ export const GET = async () => {
     // buat variabel menampilkan data barang
     const barang = await prisma.tb_barang.findMany({
         orderBy: {
-            kode: "asc"
+            id: "asc"
         },
-        where: {
-            kode: "KY12"
-        }
+        // where: {
+        //     kode: "KY12"
+        // }
     });
 
     // tampilkan hasil data barang
-    return NextResponse.json(barang);
+    return NextResponse.json({
+        barang: barang
+    });
 
+}
+
+// buat service POST (simpan data)
+export const POST = async (request: NextRequest) => {
+    // simpan data
+    const data = await request.json();
+    await prisma.tb_barang.create({
+        data: {
+            kode: data.kode,
+            nama: data.nama,
+            harga: data.harga,
+            satuan: data.satuan
+        }       
+    })
+
+    return NextResponse.json({
+        message: "Data barang berhasil disimpan",
+        success: true
+    })
 }
